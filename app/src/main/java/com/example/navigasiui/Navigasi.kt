50005bf5cc5eb81c7fce0aa5433026fa12a4fb09
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-06 17:20:30 +0700

    Setup Navigasi dengan enum class dan NavHost

--- app/src/main/java/com/example/navigasiui/Navigasi.kt
+++ app/src/main/java/com/example/navigasiui/Navigasi.kt
@@ -10,3 +10,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.padding
 import com.example.navigasiui.view.FormIsian
 import com.example.questnavigasiui_235.view.TampilData
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
