090652ecd247d4778c79007b3e1075779f4f36d6
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-06 17:36:19 +0700

    Tambah parameter onBackBtnClick dan dummy data TampilData

--- app/src/main/java/com/example/navigasiui/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiui/view/TampilData.kt
@@ -19,3 +19,11 @@ import com.example.navigasiui.R
 @Composable
 fun TampilData(
 
+    onBackBtnClick: () -> Unit
+) {
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+
