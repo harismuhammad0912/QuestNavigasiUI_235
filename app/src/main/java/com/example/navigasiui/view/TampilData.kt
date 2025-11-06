d4a5006e6ab93589825f681d907c522009174f19
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-06 17:37:58 +0700

    Tampilkan label data (item.first) dalam TampilData

--- app/src/main/java/com/example/navigasiui/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiui/view/TampilData.kt
@@ -50,3 +50,10 @@ fun TampilData(
                 verticalArrangement = Arrangement.spacedBy(
                     space = dimensionResource(id = R.dimen.padding_small)
                 )
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(
+                            text = item.first.uppercase(),
+                            fontSize = 16.sp
+                        )
