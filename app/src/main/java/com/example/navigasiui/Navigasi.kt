80c48decc9e96351ec0c92837cf16ed00831019a
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-06 17:18:11 +0700

    NavigasiUI

---
+++ app/src/main/java/com/example/navigasiui/Navigasi.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigasiui_235
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
+import com.example.navigasiui.view.FormIsian
+import com.example.questnavigasiui_235.view.TampilData
