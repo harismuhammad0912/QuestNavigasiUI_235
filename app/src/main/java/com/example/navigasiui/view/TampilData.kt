33a3ba72997f8848821e372b96f673e974a5b3da
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-11-06 17:35:07 +0700

    Tambah import untuk Composable TampilData

--- app/src/main/java/com/example/navigasiui/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiui/view/TampilData.kt
@@ -1,1 +1,16 @@
 package com.example.questnavigasiui_235.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navigasiui.R
